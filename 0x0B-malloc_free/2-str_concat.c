a7e2ab0d20ca90bb10d4f2c7fd2aa3227694ca03
simon muzariri <simonsammuzariri@gmail.com>
2023-06-13 13:49:36 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,3 +7,35 @@
  * Return: pointer to a new memory, NULL
  */
 char *str_concat(char *s1, char *s2)
+{
+	char *result;
+	int i, ci;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	i = ci = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	result = malloc(sizeof(char) * (i + ci + 1));
+	if (result == NULL)
+		return (NULL);
+	i = ci = 0;
+	while (s1[i] != '\0')
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	while (s2[ci] != '\0')
+	{
+		result[i] = s2[ci];
+		i++, ci++;
+	}
+	result[i] = '\0';
+	return (result);
+}
