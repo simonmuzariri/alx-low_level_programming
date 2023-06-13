12b79431d021b089b183269bb500a6f400fe1591
simon muzariri <simonsammuzariri@gmail.com>
2023-06-13 05:01:52 -0700

    0-creat

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,12 @@ char *create_array(unsigned int size, char c)
 	char *str;
 	unsigned int i;
 
-	str = malloc(sizeof(char) * size);
-	if (str == 0 || str == NULL)
+	if (size == 0)
 		return (NULL);
-	for (i = 0; i < size; i++)
-		str[i] = c;
-	return (str);
+	str = (char *)malloc(size * sizeof(char));
+			if (str == NULL)
+			return (NULL);
+			for (i = 0; i < size; i++)
+			str[i] = c;
+			return (str);
 }
